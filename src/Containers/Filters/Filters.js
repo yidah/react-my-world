9aa7240629eabefdcaa0e6d5b081213960dcc2af
Yidah <yidahit@hotmail.com>
2021-03-02 11:50:03 +0000

    Got Filters with Map css desktop ready for API testiing

--- src/Containers/Filters/Filters.js
+++ src/Containers/Filters/Filters.js
@@ -5,13 +5,13 @@ class Filters extends Component {
   render() {
     return (
       <div className={classes.Filters}>
-        <div className={classes.formstyle10}>
-          <h1>Merida</h1>
+        <div className={classes.FiltersForm}>
+          <h1 className={classes.FiltersExploredPlace}>Merida</h1>
           <form>
-            <div className={classes.section}>
+            <div className={classes.FiltersSectionTitle}>
               <span>1</span>Search for nearby places
             </div>
-            <div className={classes.innerwrap}>
+            <div className={classes.FiltersSectionContent}>
               <input type="text" name="field1" />
               <input
                 type="submit"
@@ -20,20 +20,21 @@ class Filters extends Component {
                 value="Go"
               />
             </div>
-            <div className={classes.section}>
+            <div className={classes.FiltersSectionTitle}>
               <span>2</span>Draw a shape to search within it for places
             </div>
-            <div className={classes.innerwrap}>
+            <div className={classes.FiltersSectionContent}>
               <input type="submit" name="DrawingTools" value="Drawing Tools" />
             </div>
 
-            <div className={classes.section}>
+            <div className={classes.FiltersSectionTitle}>
               <span>3</span>Enter your favourite area
             </div>
 
-            <div className={classes.innerwrap}>
-              <div className={classes.favoriteArea}>
-                <div className={classes.subgroup}>
+            <div className={classes.FiltersSectionContent}>
+              <div className={classes.FiltersSectionContentMultiGroup}>
+
+                <div className={classes.FiltersSectionContentMultiGroupSubGroup}>
                   <input type="text" name="field1" />
                   <input
                     type="submit"
@@ -43,8 +44,8 @@ class Filters extends Component {
                   />
                 </div>
 
-                <div className={classes.distance}>
-                  <div className={classes.subgroup}>
+                <div className={classes.FiltersSectionContentMultiGroup}>
+                  <div className={classes.FiltersSectionContentMultiGroupSubGroup}>
                     <label for="distance">Within:</label>
                     <select name="distance" id="distance">
                       <option value="10">10 min</option>
@@ -52,9 +53,6 @@ class Filters extends Component {
                       <option value="30">30 min</option>
                       <option value="1">1 hour</option>
                     </select>
-                  </div>
-
-                  <div className={classes.subgroup}>
                     <select name="transit" id="transit">
                       <option value="drive">drive</option>
                       <option value="walk">walk</option>
@@ -66,7 +64,7 @@ class Filters extends Component {
 
                 </div>
 
-                <div className={classes.subgroup}>
+                <div className={classes.FiltersSectionContentMultiGroupSubGroup}>
                   <input type="text" name="field1" />
                   <input
                     type="submit"
